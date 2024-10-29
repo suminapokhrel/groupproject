80a42c24ffc12587f7b02b6832171925cde6c5e4
Sumina Pokhrel <pokhrel.sumina66@gmail.com>
2024-10-29 22:43:29 +0000

    deleted extra file

--- problem3.py
+++ problem3.py
@@ -1,6 +1,6 @@
 def find_largest_number(numbers):
     if not numbers:
-        return None #returns none if the list is empty
+        return None #returns none if list is empty
     
     largest = numbers [0]
     for numbers in numbers:
