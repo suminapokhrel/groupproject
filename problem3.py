aa32304bb6efad59445cc60f4e7d7c5aee692f81
Sumina Pokhrel <pokhrel.sumina66@gmail.com>
2024-10-29 22:38:23 +0000

    creates function to find largest number

--- problem3.py
+++ problem3.py
@@ -1,1 +1,11 @@
+def find_largest_number(numbers):
+    if not numbers:
+        return None #returns none if the list is empty
+    
+    largest = numbers [0]
+    for numbers in numbers:
+        if number > largest:
+            largest = number
+    return largest
+
 
